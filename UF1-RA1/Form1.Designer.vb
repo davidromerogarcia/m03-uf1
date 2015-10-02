508d608f80174a2f380bec96fa92e1d0cf879fc4
davidromerogarcia <davicetin@gmail.com>
2015-10-02 18:52:14 +0200

    first commit

---
+++ UF1-RA1/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Sumabtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Restart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Sumabtn
+        '
+        Me.Sumabtn.Location = New System.Drawing.Point(25, 141)
+        Me.Sumabtn.Name = "Sumabtn"
+        Me.Sumabtn.Size = New System.Drawing.Size(75, 23)
+        Me.Sumabtn.TabIndex = 0
+        Me.Sumabtn.Text = "Sumar"
+        Me.Sumabtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(120, 44)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(70, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(120, 93)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(70, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(120, 138)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 26)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "            "
+        '
+        'Restart
+        '
+        Me.Restart.Location = New System.Drawing.Point(197, 141)
+        Me.Restart.Name = "Restart"
+        Me.Restart.Size = New System.Drawing.Size(75, 23)
+        Me.Restart.TabIndex = 4
+        Me.Restart.Text = "Restar"
+        Me.Restart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Restart)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Sumabtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Sumabtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Restart As Button
+End Class
---
+++ UF1-RA1/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Dim op1 As Integer
+    Dim op2 As Integer
+    Dim resultado As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Sumabtn.Click
+
+        op1 = CInt(TextBox1.Text)
+        op2 = CInt(TextBox2.Text)
+        resultado = op1 + op2
+        Label1.Text = resultado.ToString
+
+    End Sub
+
+    Private Sub Restart_Click(sender As Object, e As EventArgs) Handles Restart.Click
+        op1 = CInt(TextBox1.Text)
+        op2 = CInt(TextBox2.Text)
+        resultado = op1 - op2
+        Label1.Text = resultado.ToString
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
